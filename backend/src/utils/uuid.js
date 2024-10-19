96ac51dd42db44ded028e3c5f1284588f2fc6fbd
Charles Stello <cstello@protonmail.com>
2024-10-19 11:40:53 -0400

    dotenv updates

--- backend/src/utils/uuid.js
+++ backend/src/utils/uuid.js
@@ -1,18 +1,18 @@
-import {v5 as uuidv5} from "uuid";
+import { v5 as uuidv5 } from "uuid";
 
 // Crypto
 import crypto from "crypto";
 
 // Dotenv
 import dotenv from "dotenv";
-dotenv.config({path: "../../../.env"});
+dotenv.config();
 
 const namespace = process.env.UUID_NAMESPACE;
 
 export const generateUUID = (email) => {
     // Generate random characters to ensure even repeat emails get a new uuid to stop shit from breaking
     const randomBytes = crypto.randomBytes(4).toString("hex");
-    const randomizedEmail = email+randomBytes;
+    const randomizedEmail = email + randomBytes;
 
     return uuidv5(randomizedEmail, namespace);
 };
