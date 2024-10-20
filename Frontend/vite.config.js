06d254ce376fa826d440f26a117eaf20bc10d22d
Charles Stello <cstello@protonmail.com>
2024-10-20 12:08:06 -0400

    removed remix dev tools

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import { remixDevTools } from "remix-development-tools";
 
 export default defineConfig({
-    plugins: [remixDevTools(), 
+    plugins: [
         remix({
             appDirectory: "app",
             buildDirectory: "build",
